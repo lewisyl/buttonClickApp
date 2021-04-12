b5ca1f6596080449b4e5a956ce7c7b1138842727
Lewis Liu <lewisliuusa@outlook.com>
2021-04-11 21:45:20 -0700

    added onClickListener

--- app/src/main/java/me/lewisliu/buttonclickapp/MainActivity.java
+++ app/src/main/java/me/lewisliu/buttonclickapp/MainActivity.java
@@ -3,12 +3,35 @@ package me.lewisliu.buttonclickapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText userInput;
+    private Button button;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userInput = findViewById(R.id.userInput);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+        textView.setText("");
+        textView.setMovementMethod(new ScrollingMovementMethod());
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               String result = userInput.getText().toString();
+               result += "\n";
+               textView.append(result);
+            }
+        };
+        button.setOnClickListener(onClickListener);
     }
 }
